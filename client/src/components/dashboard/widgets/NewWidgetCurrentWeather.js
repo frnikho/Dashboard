ae4a980a1c83a1cf70f50db847ee3a1f02f3ec56
Victor <victor.sauvaget@epitech.eu>
2021-11-30 17:44:02 +0100

    NewWidgetCurrentWeather add icon weather in function of weather

--- client/src/components/dashboard/widgets/NewWidgetCurrentWeather.js
+++ client/src/components/dashboard/widgets/NewWidgetCurrentWeather.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Widget from "./Widget";
 import app from "../../../config/axiosConfig";
-import {FaCloud} from "react-icons/all";
 import {CircularProgress} from "@mui/material";
 
 export default class NewWidgetCurrentWeather extends Widget {
@@ -49,7 +48,7 @@ export default class NewWidgetCurrentWeather extends Widget {
             return <CircularProgress/>;
         return (
             <div>
-                <FaCloud size={"40"}/>
+                <img alt="weather icon" src={"http://openweathermap.org/img/wn/" + this.state.weather.weather[0].icon + "@2x.png"}/>
                 <h3>{this.state.weather.name}</h3>
                 <h2>{this.state.weather.main.temp}°</h2>
                 <h4>({this.state.weather.main.temp_min}° / {this.state.weather.main.temp_max}°)</h4>
