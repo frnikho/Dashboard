c0651541fb62625c9289f26124df2d3499c0ed6d
Nicolas SANS <nicolas.sansd@gmail.com>
2021-12-03 09:20:07 +0100

    updated CI secrets

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.DB_PORT || 8080;
 
 const options = {
     definition: {
--- server/src/services/DBService.js
+++ server/src/services/DBService.js
@@ -1,7 +1,7 @@
 const mariadb = require('mariadb');
 
 module.exports = mariadb.createPool({
-   host: "172.19.0.1",
+   host: process.env.DB_HOST,
    user: process.env.DB_USER,
    password: process.env.DB_PASSWORD,
    database: process.env.DB_DBNAME,
